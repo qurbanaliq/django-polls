66e3d1bea95fda5fe93a7974f3f8b3e8670463aa
Qurban Ali <qurbanaliqureshi@gmail.com>
2021-12-18 20:47:28 +0000

    generic views

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,23 @@ from .models import Question, Choice
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_questions"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def index(request):
     latest_questions = Question.objects.order_by("-pub_date")[:5]
