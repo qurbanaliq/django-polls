c5eae943ff3650fcc5bf2c533ba84d71cf64be28
Qurban Ali <qurbanaliqureshi@gmail.com>
2021-12-19 22:46:23 +0000

    tests added for detail view

--- polls/tests.py
+++ polls/tests.py
@@ -81,3 +81,20 @@ class QuestionIndexViewTestCase(TestCase):
         response = self.client.get(reverse("polls:index"))
         self.assertQuerysetEqual(response.context["latest_questions"],
             [past_question2, past_question1])
+
+class QuestionDetailViewTestCase(TestCase):
+    def test_future_question(self):
+        """Future questions shouldn't be displayed on detail view
+        """
+        future_question = create_question("future question", days=30)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+    
+    def test_past_question(self):
+        """past questions should be displayed on the detail view
+        """
+        past_question = create_question("past question", days=-30)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
